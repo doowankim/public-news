4c4124fec0639abf2d502b7c842de6f91e76b273
doowankim <rlaendhks11@gmail.com>
2020-10-25 18:05:05 +0900

    글로벌스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 
 function App() {
+	const GlobalStyle = createGlobalStyle`
+    body {
+      margin: 0;
+      padding: 0;
+    }
+  `;
 	return (
 		<div>
+			<GlobalStyle />
 			<div>App</div>
 		</div>
 	);
